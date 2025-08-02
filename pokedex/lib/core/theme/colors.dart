b87c23604ee5ba33a6b6ac8f258c16a07921a27c
Rudá Torquato <rudatorquato@gmail.com>
2025-08-02 17:06:52 -0300

    feat:colors

---
+++ pokedex/lib/core/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+const primarycolor = Color(0xffD4001B);
+const docAvatar = Color.fromRGBO(222, 222, 222, 1);
+
+//Colors Alerts
+const successColor = Colors.green;
+const alertColor = Colors.orange;
+const errorColor = primarycolor;
