c3a8724949e48eef5f8c379ba3dcf3b458b13e9d
Rudá Torquato <rudatorquato@gmail.com>
2025-08-04 12:14:05 -0300

    feat:new_colors

--- pokedex/lib/core/theme/colors.dart
+++ pokedex/lib/core/theme/colors.dart
@@ -4,6 +4,14 @@ import 'package:flutter/material.dart';
 const primarycolor = Color(0xffD4001B);
 const background = Color.fromRGBO(222, 222, 222, 1);
 const translucentWhite = Color.fromARGB(100, 255, 255, 255);
+const Color colorButtomBerry =  Color(0xFFFACE53);
+const Color colorButtomPokemon=  Color(0xFF57CDA3);
+const Color colorButtomMoves =  Color(0xFFE6676E);
+const Color colorButtomAbilities =  Color(0xFF67AEF0);
+const Color colorButtomType =  Color(0xFFB26E6E);
+const Color colorButtomLocate =  Color(0xFF764888);
+const Color colorPokeball =  Color(0xFFf5f3f6);
+
 //Colors Alerts
 const successColor = Colors.green;
 const alertColor = Colors.orange;
